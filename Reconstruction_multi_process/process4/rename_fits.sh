e30132090ce9d6ee703b63041d6395a4f4bc2945
Chen Wenzheng <147072404+ChenWenzheng99@users.noreply.github.com>
2024-08-29 16:15:28 +0800

    Internal reconstruction at 93GHz (in 5 groups)
    This folder is used for calculating the sim_p_xxxx.fits from 0300 to 0399.
    They will be moved to /process_added/ALILENS/temp/qlm_dd.

---
+++ Reconstruction_multi_process/process4/rename_fits.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# 设置文件夹路径
+folder_path="/sharefs/alicpt/users/chenwz/reconstruction_multi_process/process3/ALILENS/temp/qlms_dd"
+
+# 遍历文件夹中的文件并重命名
+
+for i in $(seq -w 200 299); do
+    old_name="${folder_path}/sim_p_$(printf "%04d" $i).fits"
+    new_index=$(printf "%04d" $((i - 200)))
+    new_name="${folder_path}/sim_p_${new_index}.fits"
+    
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "Renamed: $old_name to $new_name"
+    else
+        echo "File not found: $old_name"
+    fi
+done
+
+
+for i in $(seq -w 200 299); do
+    old_name="${folder_path}/sim_p_p_$(printf "%04d" $i).fits"
+    new_index=$(printf "%04d" $((i - 200)))
+    new_name="${folder_path}/sim_p_p_${new_index}.fits"
+    
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "Renamed: $old_name to $new_name"
+    else
+        echo "File not found: $old_name"
+    fi
+done
+
+
+for i in $(seq -w 200 299); do
+    old_name="${folder_path}/sim_ptt_$(printf "%04d" $i).fits"
+    new_index=$(printf "%04d" $((i - 200)))
+    new_name="${folder_path}/sim_ptt_${new_index}.fits"
+    
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "Renamed: $old_name to $new_name"
+    else
+        echo "File not found: $old_name"
+    fi
+done
+
+
+for i in $(seq -w 200 299); do
+    old_name="${folder_path}/sim_x_p_$(printf "%04d" $i).fits"
+    new_index=$(printf "%04d" $((i - 200)))
+    new_name="${folder_path}/sim_x_p_${new_index}.fits"
+    
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "Renamed: $old_name to $new_name"
+    else
+        echo "File not found: $old_name"
+    fi
+done
+
+
+for i in $(seq -w 200 299); do
+    old_name="${folder_path}/sim_x_$(printf "%04d" $i).fits"
+    new_index=$(printf "%04d" $((i - 200)))
+    new_name="${folder_path}/sim_x_${new_index}.fits"
+    
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "Renamed: $old_name to $new_name"
+    else
+        echo "File not found: $old_name"
+    fi
+done
+
+
+for i in $(seq -w 200 299); do
+    old_name="${folder_path}/sim_xtt_$(printf "%04d" $i).fits"
+    new_index=$(printf "%04d" $((i - 200)))
+    new_name="${folder_path}/sim_xtt_${new_index}.fits"
+    
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "Renamed: $old_name to $new_name"
+    else
+        echo "File not found: $old_name"
+    fi
+done
